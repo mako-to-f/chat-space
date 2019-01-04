cd4be7ff0c1b8983bde95f283f9bdc2602f40eb6
mako-to-f <makoto.fujimoto.0711ex@gmail.com>
2019-01-04 13:34:29 +0900

    modify_routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:create, :new, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
