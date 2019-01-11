a3bd84df7f9c679c36bab1c14fc508f0dcc19bb2
mako-to-f <makoto.fujimoto.0711ex@gmail.com>
2019-01-11 11:59:04 +0900

    add_ajax-done-process

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,32 @@
 $(function() {
+  function buildContentHTML(message) {
+    var html = `<div class="chat-main__message">
+                  <div class="chat-main__message-name">
+                    ${message.user_name}
+                  </div>
+                  <div class="chat-main__message-time">
+                    ${message.created_at}
+                  </div>
+                  <div class="chat-main__message-body">
+                    ${message.content}
+                  </div>
+                </div>`
+    return html;
+  }
+  function buildImageHTML(message) {
+    var html = `<div class="chat-main__message">
+                  <div class="chat-main__message-name">
+                    ${message.user_name}
+                  </div>
+                  <div class="chat-main__message-time">
+                    ${message.created_at}
+                  </div>
+                  <div class="chat-main__message-body">
+                    <img src="${message.image_url}">
+                  </div>
+                </div>`
+    return html;
+  }
   $('#new_message').submit(function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -11,6 +39,19 @@ $(function() {
       dataType:    'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data) {
+      if(data.content !== "") {
+        var html = buildContentHTML(data);
+        $('.chat-main__body--message-list').append(html)
+        $('.message_form').val('')
+      }
+      if(data.image_url !== null) {
+        var html = buildImageHTML(data);
+        $('.chat-main__body--message-list').append(html)
+        $('.message_image-file').val(null)
+      }
+      $('.submit').prop('disabled', false);
     })
   })
 })
