c02ff9b35ae5a9ba89d569233ecd781b2367684b
mako-to-f <makoto.fujimoto.0711ex@gmail.com>
2019-01-11 15:52:26 +0900

    add_asynchronous-communication_by_event-trigger

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,6 +1,12 @@
 $(function() {
   $("#user-search-field").keyup(function() {
     var input = $("#user-search-field").val();
-    console.log(input);
+
+    $.ajax( {
+      type:     'GET',
+      url:      '/users',
+      data:     { keyword: input },
+      dataType: 'json'
+    })
   });
 });
