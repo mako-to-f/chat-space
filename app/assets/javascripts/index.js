d0c83440114af114f0a569a91e3baea1fb2c62bf
mako-to-f <makoto.fujimoto.0711ex@gmail.com>
2019-01-12 19:04:57 +0900

    add_adding-user_by_event-trigger

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -2,6 +2,7 @@ $(function() {
 
   var preInput;
   var search_list = $("#user-search-result");
+  var member_list = $('#chat-group-users')
 
   function appendUser(user) {
     var html = `<div class="chat-group-user clearfix">
@@ -15,6 +16,14 @@ $(function() {
                   <p class="chat-group-user__name">${ user }</p>
                 </div>`
     search_list.append(html);
+  }
+  function appendMember(userId, userName) {
+    var html = `<div class="chat-group-user clearfix js-chat-member" id="chat-group-user-8">
+                  <input name="group[user_ids][]" type="hidden" value="${ userId }">
+                  <p class="chat-group-user__name">${ userName }</p>
+                  <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn">削除</a>
+                </div>`
+    member_list.append(html);
   }
 
   $("#user-search-field").keyup(function() {
@@ -49,6 +58,14 @@ $(function() {
   });
 
   $('.chat-group-form__field--right').on('click', '.user-search-add', function() {
-    console.log('ok')
+    var eventTaget = event.target
+    var eventParent = eventTaget.parentNode
+    eventParent.remove();
+    var userId = eventTaget.getAttribute('data-user-id');
+    var userName = eventTaget.getAttribute('data-user-name');
+    appendMember(userId, userName);
+
+
+    console.log(dataUserName);
   })
 });
